2b3bec9d23eeb52469aed7e53c1ec468a7a03bd6
Momo Saranjam <msarajam@gmail.com>
2019-10-18 18:13:41 -0700

    adding switch for controller

--- TestOne/src/main.cpp
+++ TestOne/src/main.cpp
@@ -22,7 +22,9 @@ using namespace vex;
 
 int motorSpeed = 100;
 int elevSpeed = 100;
+bool controllerSwitch = false;
 
+// SpeedController is for controlling the speed of motor
 int SpeedController() {
   Brain.Screen.clearLine(2, color::black);
   Brain.Screen.setCursor(2, 0);
@@ -57,6 +59,21 @@ int SpeedController() {
   return 0;
 }
 
+// ControllerSwitch is for switch between joystin and button , just for driving
+int ControllerSwitch() {
+  while (true) {
+    if (Controller1.ButtonL1.pressing() && Controller1.ButtonL2.pressing()) {
+      if (controllerSwitch) {
+        controllerSwitch = false;
+      } else {
+        controllerSwitch = true;
+      }
+    }
+    vex::task::sleep(100);
+  }
+  return 0;
+}
+// grabberController is for control the motion of grabber
 void grabberController() {
   if (Controller1.ButtonUp.pressing()) {
     grabberML.spin(forward);
@@ -70,8 +87,9 @@ void grabberController() {
   }
 }
 
+// elevatorController is for control the motion of elevator
 void elevatorController() {
-  if (Controller1.ButtonRight.pressing()) {
+  if (Controller1.ButtonRight.pressing() && Controller1.ButtonLeft.pressing()) {
     if (ElevM.position(degrees) < 2100) {
       motorSpeed = 10;
       elevSpeed = 100;
@@ -85,6 +103,7 @@ void elevatorController() {
       vex::task::sleep(100);
       grabberML.spin(forward);
       grabberMR.spin(forward);
+      elevSpeed = 70;
       ElevM.spinToPosition(2250, degrees);
       vex::task::sleep(500);
       grabberML.stop();
@@ -95,6 +114,8 @@ void elevatorController() {
     if (ElevM.position(degrees) > 50) {
       ElevM.spinToPosition(0, degrees);
     }
+  } else if (Controller1.ButtonRight.pressing()) {
+    ElevM.spin(forward);
   } else {
     ElevM.stop();
   }
@@ -108,7 +129,10 @@ int main() {
   Brain.Screen.setCursor(1, 0);
   Brain.Screen.print("Prigram Starteed");
   ElevM.setPosition(0, degrees);
-  vex::task newtask(SpeedController);
+
+  //running tasks
+  vex::task speedController(SpeedController);
+  vex::task controllerSwitch(ControllerSwitch);
 
   while (true) {
     grabberController();
